31e3bd64643dd935ffe94c6a494aa7a6aff1040e
Rose Cox <rose@purpleprodigy.com>
2016-05-18 13:31:10 +1000

    fixed PDF to show value for scores

--- uploads/PDF_EXTENDED_TEMPLATES/ras-ds-online-custom-template.php
+++ uploads/PDF_EXTENDED_TEMPLATES/ras-ds-online-custom-template.php
@@ -73,32 +73,32 @@ $form = RGFormsModel::get_form_meta($form_id);
 	        <tbody>
 	        <tr>
 		        <td><span id="q1-question">1: It is important to have fun</span></td>
-		        <td>{1: It is important to have fun:4}</td>
+		        <td>{1: It is important to have fun:4:value}</td>
 		        <td><em>{Comment:5}</em></td>
 	        </tr>
 	        <tr>
 		        <td><span id="q2-question">2: It is important to have healthy habits</span></td>
-		        <td>{2: It is important to have healthy habits:6}</td>
+		        <td>{2: It is important to have healthy habits:6:value}</td>
 		        <td><em>{Comment:7}</em></td>
 	        </tr>
 	        <tr>
 		        <td><span id="q3-question">3: I do things that are meaningful to me</span></td>
-		        <td>{3: I do things that are meaningful to me:8}</td>
+		        <td>{3: I do things that are meaningful to me:8:value}</td>
 		        <td><em>{Comment:9}</em></td>
 	        </tr>
 	        <tr>
 		        <td><span id="q4-question">4: I continue to have new interests</span></td>
-		        <td>{4: I continue to have new interests:10}</td>
+		        <td>{4: I continue to have new interests:10:value}</td>
 		        <td><em>{Comment:11}</em></td>
 	        </tr>
 	        <tr>
 		        <td><span id="q5-question">5: I do things that are valuable and helpful to others</span></td>
-		        <td>{5: I do things that are valuable and helpful to others:12}</td>
+		        <td>{5: I do things that are valuable and helpful to others:12:value}</td>
 		        <td><em>{Comment:13}</em></td>
 	        </tr>
 	        <tr>
 		        <td><span id="q6-question">6: I do things that give me a feeling of great pleasure</span></td>
-		        <td>{6: I do things that give me a feeling of great pleasure:14}</td>
+		        <td>{6: I do things that give me a feeling of great pleasure:14:value}</td>
 		        <td><em>{Comment:15}</em></td>
 	        </tr>
 	        <tr>
@@ -128,92 +128,92 @@ $form = RGFormsModel::get_form_meta($form_id);
 		        <tbody>
 		        <tr>
 			        <td><span id="q7-question">7: I can handle it if I get unwell again</span></td>
-			        <td>{7: I can handle it if I get unwell again:18}</td>
+			        <td>{7: I can handle it if I get unwell again:18:value}</td>
 			        <td><em>{Comment:19}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q8-question">8: I can help myself become better</span></td>
-			        <td>{8: I can help myself become better:20}</td>
+			        <td>{8: I can help myself become better:20:value}</td>
 			        <td><em>{Comment:21}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q9-question">9: I have the desire to succeed</span></td>
-			        <td>{9: I have the desire to succeed:22}</td>
+			        <td>{9: I have the desire to succeed:22:value}</td>
 			        <td><em>{Comment:23}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q10-question">10: I have goals in life that I want to reach</span></td>
-			        <td>{10: I have goals in life that I want to reach:24}</td>
+			        <td>{10: I have goals in life that I want to reach:24:value}</td>
 			        <td><em>{Comment:25}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q11-question">11: I believe that I can reach my current personal goals</span></td>
-			        <td>{11: I believe that I can reach my current personal goals:26}</td>
+			        <td>{11: I believe that I can reach my current personal goals:26:value}</td>
 			        <td><em>{Comment:27}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q12-question">12: I can handle what happens in my life</span></td>
-			        <td>{12: I can handle what happens in my life:28}</td>
+			        <td>{12: I can handle what happens in my life:28:value}</td>
 			        <td><em>{Comment:29}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q13-question">13: I like myself</span></td>
-			        <td>{13: I like myself:30}</td>
+			        <td>{13: I like myself:30:value}</td>
 			        <td><em>{Comment:31}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q14-question">14: I have a purpose in life</span></td>
-			        <td>{14: I have a purpose in life:32}</td>
+			        <td>{14: I have a purpose in life:32:value}</td>
 			        <td>{Comment:33}</td>
 		        </tr>
 		        <tr>
 			        <td><span id="q15-question">15: If people really knew me they would like me</span></td>
-			        <td>{15: If people really knew me they would like me:34}</td>
+			        <td>{15: If people really knew me they would like me:34:value}</td>
 			        <td><em>{Comment:35}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q16-question">16: If I keep trying, I will continue to get better</span></td>
-			        <td>{16: If I keep trying, I will continue to get better:36} </td>
+			        <td>{16: If I keep trying, I will continue to get better:36:value} </td>
 			        <td><em>{Comment:37}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q17-question">17: I have an idea of who I want to become</span></td>
-			        <td>{17: I have an idea of who I want to become:38}</td>
+			        <td>{17: I have an idea of who I want to become:38:value}</td>
 			        <td><em>{Comment:39}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q18-question">18: Something good will eventually happen</span></td>
-			        <td>{18: Something good will eventually happen:40}</td>
+			        <td>{18: Something good will eventually happen:40:value}</td>
 			        <td><em>{Comment:41}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q19-question">19: I am the person most responsible for my own improvement</span></td>
-			        <td>{19: I am the person most responsible for my own improvement:42}</td>
+			        <td>{19: I am the person most responsible for my own improvement:42:value}</td>
 			        <td><em>{Comment:43}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q20-question">20: I am hopeful about my own future</span></td>
-			        <td>{20: I am hopeful about my own future:44}</td>
+			        <td>{20: I am hopeful about my own future:44:value}</td>
 			        <td><em>{Comment:45}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q21-question">21: I know when to ask for help</span></td>
-			        <td>{21: I know when to ask for help:46}</td>
+			        <td>{21: I know when to ask for help:46:value}</td>
 			        <td><em>{Comment:47}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q22-question">22: I ask for help, when I need it</span></td>
-			        <td>{22: I ask for help, when I need it:48}</td>
+			        <td>{22: I ask for help, when I need it:48:value}</td>
 			        <td><em>{Comment:49}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q23-question">23: I know what helps me get better</span></td>
-			        <td>{23: I know what helps me get better:50}</td>
+			        <td>{23: I know what helps me get better:50:value}</td>
 			        <td><em>{Comment:51}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q24-question">24: I can learn from my mistakes</span></td>
-			        <td>{24: I can learn from my mistakes:52}</td>
+			        <td>{24: I can learn from my mistakes:52:value}</td>
 			        <td><em>{Comment:53}</em></td>
 		        </tr>
 		        <tr>
@@ -244,42 +244,42 @@ $form = RGFormsModel::get_form_meta($form_id);
 		        <tbody>
 		        <tr>
 			        <td><span id="q25-question">25: I can identify the early warning signs of becoming unwell</span></td>
-			        <td>{25: I can identify the early warning signs of becoming unwell:56}</td>
+			        <td>{25: I can identify the early warning signs of becoming unwell:56:value}</td>
 			        <td><em>{Comment:57}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q26-question">26: I have my own plan for how to stay or become well</span></td>
-			        <td>{26: I have my own plan for how to stay or become well:58}</td>
+			        <td>{26: I have my own plan for how to stay or become well:58:value}</td>
 			        <td><em>{Comment:59}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q27-question">27: There are things that I can do that help me deal with unwanted symptoms</span></td>
-			        <td>{27: There are things that I can do that help me deal with unwanted symptoms:60}</td>
+			        <td>{27: There are things that I can do that help me deal with unwanted symptoms:60:value}</td>
 			        <td><em>{Comment:61}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q28-question">28. I know that there are mental health services that help me</span></td>
-			        <td>{28: I know that there are mental health services that help me:62} </td>
+			        <td>{28: I know that there are mental health services that help me:62:value} </td>
 			        <td><em>{Comment:63}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q29-question">29. Although my symptoms may get worse, I know I can handle it</span></td>
-			        <td>{29: Although my symptoms may get worse, I know I can handle it:64} </td>
+			        <td>{29: Although my symptoms may get worse, I know I can handle it:64:value} </td>
 			        <td><em>{Comment:65}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q30-question">30. My symptoms interfere less and less with my life</span></td>
-			        <td>{30: My symptoms interfere less and less with my life:66}</td>
+			        <td>{30: My symptoms interfere less and less with my life:66:value}</td>
 			        <td><em>{Comment:67}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q31-question">31. My symptoms seem to be a problem for shorter periods of</span></td>
-			        <td>{31: My symptoms seem to be a problem for shorter periods of time each time they occur:68}</td>
+			        <td>{31: My symptoms seem to be a problem for shorter periods of time each time they occur:68:value}</td>
 			        <td><em>{Comment:69}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q32-question">32. I have people that I can count on</span></td>
-			        <td>{32: I have people that I can count on:72}</td>
+			        <td>{32: I have people that I can count on:72:value}</td>
 			        <td><em>{Comment:73}</em></td>
 		        </tr>
 		        <tr>
@@ -310,32 +310,32 @@ $form = RGFormsModel::get_form_meta($form_id);
 		        <tbody>
 		        <tr>
 			        <td><span id="q33-question">33. Even when I don’t believe in myself, other people do</span></td>
-			        <td>{33: Even when I don’t believe in myself, other people do:74}</td>
+			        <td>{33: Even when I don’t believe in myself, other people do:74:value}</td>
 			        <td><em>{Comment:75}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q34-question">34. It is important to have a variety of friends</span></td>
-			        <td>{34: It is important to have a variety of friends:76}</td>
+			        <td>{34: It is important to have a variety of friends:76:value}</td>
 			        <td><em>{Comment:77}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q35-question">35. I have friends who have also experienced mental illness</span></td>
-			        <td>{35: I have friends who have also experienced mental illness:78}</td>
+			        <td>{35: I have friends who have also experienced mental illness:78:value}</td>
 			        <td><em>{Comment:79}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q36-question">36. I have friends without mental illness</span></td>
-			        <td>{36: I have friends without mental illness:80}</td>
+			        <td>{36: I have friends without mental illness:80:value}</td>
 			        <td><em>{Comment:81}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q37-question">37. I have friends that can depend on me</span></td>
-			        <td>{37: I have friends that can depend on me:82}</td>
+			        <td>{37: I have friends that can depend on me:82:value}</td>
 			        <td><em>{Comment:83}</em></td>
 		        </tr>
 		        <tr>
 			        <td><span id="q38-question">38. I feel OK about my family situation</span></td>
-			        <td>{38: I feel OK about my family situation:84}</td>
+			        <td>{38: I feel OK about my family situation:84:value}</td>
 			        <td><em>{Comment:85}</em></td>
 		        </tr>
 		        <tr>
