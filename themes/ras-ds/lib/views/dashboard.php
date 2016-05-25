d8e909e5c38bab67a97b4fd9077f9ec173509757
Naomi C. Bush <dev@naomicbush.com>
2016-05-25 06:43:33 -0400

    Remove dashboard info from single entry view
    A GravityView and all of its configurations are displayed in whatever page template the shortcode is added to — in this case, the dashboard template. However, a single entry has its own layout and should not be displayed with the main dashboard information.
    
    This commit adjusts the dashboard template to only show the the dashboard information if a single entry is not being displayed, using the GravityView query variable.

--- themes/ras-ds/lib/views/dashboard.php
+++ themes/ras-ds/lib/views/dashboard.php
@@ -2,6 +2,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-12">
+				<?php if ( empty( $_GET['gvid'] ) ) { ?>
 				<h2 class="mar-0 mar-b-4 text-9">My RAS-DS Tracker: <span class="text-regular"><?php echo do_shortcode('[gravityform action="user" key="nickname"]'); ?></span></h2>
 				<div class="row">
 					<div class="col-sm-5 pad-b-3">
@@ -22,10 +23,16 @@
 						</ul>
 						<h3 class="mar-0 mar-b-2">Completed RAS-DS</h3>
 						<ul class="list-icon fa-ul mar-b-4">
-							<li class="mar-b-1"><i class="fa-li fa fa-file-text-o c-mid-gray"></i><?php echo do_shortcode('[gravityview id=\'91\']'); ?></li>
+							<li class="mar-b-1"><i class="fa-li fa fa-file-text-o c-mid-gray"></i><?php echo do_shortcode('[gravityview id="321"]'); ?></li>
 						</ul>
 					</div>
 				</div>
+				<?php } else { ?>
+				<div class="row">
+					<?php echo do_shortcode('[gravityview id=\'227\']'); ?>
+					<?php echo do_shortcode('[gravityview id=\'321\']'); ?>
+				</div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
