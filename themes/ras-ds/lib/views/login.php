f558de0b5fb2c1e4c153ad026b1410e7102c2604
Rose Cox <rose@purpleprodigy.com>
2016-06-09 10:34:24 +1000

    remove 'add form here'

--- themes/ras-ds/lib/views/login.php
+++ themes/ras-ds/lib/views/login.php
@@ -4,7 +4,7 @@
 			<div class="col-sm-10 col-sm-offset-2">
 				<h2 class="mar-0 mar-b-4 text-9"><?php esc_html_e( $page->post_title ); ?></h2>
 				<div class="row">
-					<div class="col-sm-7">Add form here
+					<div class="col-sm-7">
 					</div>
 				</div>
 			</div>
