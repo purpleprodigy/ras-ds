913db693e1971747ad7683b61880d35d6ba0e81c
Rose Cox <rose@purpleprodigy.com>
2016-06-17 14:41:24 +1000

    Rename all occurrences of 'test' to 'RAS-DS'

--- themes/ras-ds/lib/views/compare.php
+++ themes/ras-ds/lib/views/compare.php
@@ -4,7 +4,7 @@
 			<div class="col-sm-8 col-sm-offset-2">
 				<div class="mar-b-3 overflow-hidden"><a class="float-left btn btn-default mar-r-2" href="/rasds/dashboard">Back to My Tracker</a></div>
 
-				<h2 class="mar-0 mar-b-4 text-9">Compare Other Completed Tests</h2>
+				<h2 class="mar-0 mar-b-4 text-9">Compare Other Completed RAS-DS</h2>
 
 				<div class="row">
 					<div class="col-sm-7">
@@ -16,18 +16,18 @@
 						<form id="rasds-comparison-form">
 							<div class="error"></div>
 							<div class="form-group">
-								<label for="rasds-compare-test1">Test 1:</label>
+								<label for="rasds-compare-test1">RAS-DS 1:</label>
 								<select id="rasds-compare-test1" class="form-control">
-									<option value="">Choose test</option>
+									<option value="">Choose RAS-DS</option>
 									<?php foreach( $entries_list as $entry ) { ?>
 										<option value="<?php echo $entry['id']?>"><?php echo $entry['label']?></option>
 									<?php } unset( $entry );?>
 								</select>
 							</div>
 							<div class="form-group">
-								<label for="rasds-compare-test2">Test 2:</label>
+								<label for="rasds-compare-test2">RAS-DS 2:</label>
 								<select id="rasds-compare-test2" class="form-control">
-									<option value="">Choose test</option>
+									<option value="">Choose RAS-DS</option>
 									<?php foreach( $entries_list as $entry ) { ?>
 										<option value="<?php echo $entry['id']?>"><?php echo $entry['label']?></option>
 									<?php } unset( $entry );?>
