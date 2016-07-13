d00e48d4d7d9fa5b86784570510c37c2e1c97244
Rose Cox <rose@purpleprodigy.com>
2016-07-13 11:11:53 +1000

    Uncomment enqueue script to responsive-nav.js and remove npm.js completely

--- themes/ras-ds/lib/functions/enqueue-assets.php
+++ themes/ras-ds/lib/functions/enqueue-assets.php
@@ -23,7 +23,7 @@ function enqueue_assets() {
 	wp_enqueue_style( 'print', get_stylesheet_directory_uri() . '/assets/css/print.css', '', '', 'print' );
 	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/assets/css/font-awesome.min.css' );
 	wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/assets/js/bootstrap.js', array() );
-//	wp_enqueue_script( 'responsive-nav', get_stylesheet_directory_uri() . '/assets/js/responsive-nav.js', array( 'jquery' ) );
+	wp_enqueue_script( 'responsive-nav', get_stylesheet_directory_uri() . '/assets/js/responsive-nav.js', array( 'jquery' ) );
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,800,800italic,400italic', array(), CHILD_THEME_VERSION );
 }
 
